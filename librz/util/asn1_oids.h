1211c0d630d52e77ab790bbf9b062098f359c0d0
Anton Kochkov <anton.kochkov@gmail.com>
2021-03-29 14:27:35 +0800

    Update missing copyrights

--- librz/util/asn1_oids.h
+++ librz/util/asn1_oids.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2018 pancake <pancake@nopcode.org>
+// SPDX-FileCopyrightText: 2018 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef OIDS_H
 #define OIDS_H
 
--- librz/util/x509.h
+++ librz/util/x509.h
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2018 pancake <pancake@nopcode.org>
+// SPDX-FileCopyrightText: 2018 deroad <wargio@libero.it>
+// SPDX-License-Identifier: LGPL-3.0-only
+
 #ifndef RZ_X509_INTERNAL_H
 #define RZ_X509_INTERNAL_H
 
